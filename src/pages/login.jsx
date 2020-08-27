aa882693dcdf9b49c111bf8d34386a7bab8ff18d
Daniel Don <dandons25@gmail.com>
2020-08-27 23:52:40 +0100

    add new changes to login and registration forms

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import { Form, Container, Row, Col, Button, Toast } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../actions/userActions";
@@ -7,11 +7,11 @@ import { useSelector } from "react-redux";
 
 import "./css/forms.css";
 
-const Login = props => {
+const Login = (props) => {
   const dispatchRef = useDispatch();
   const [inputs, setInputs] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
   const closeToast = () => {
@@ -21,18 +21,18 @@ const Login = props => {
   const [showErrorToast, setShowErrorToast] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState("");
 
-  const onChangeInput = e => {
+  const onChangeInput = (e) => {
     const { name, value } = e.target;
     setInputs({ ...inputs, [name]: value });
     console.log(inputs.name);
   };
 
-  const formSubmit = e => {
+  const formSubmit = (e) => {
     e.preventDefault();
     const { email, password } = inputs;
     const loginData = {
       email,
-      password
+      password,
     };
     if (!email) {
       setShowErrorMessage("A valid Email input is required!");
@@ -44,102 +44,82 @@ const Login = props => {
       return;
     }
     loginUser(loginData, dispatchRef);
-    return(
-      <Redirect to="/dashboard"/>
-    )
+    return <Redirect to="/dashboard" />;
   };
 
-  const userState = useSelector(state => state.user);
+  const userState = useSelector((state) => state.user);
 
   if (userState.token) {
     return <Redirect to="/dashboard" />;
   }
   return (
     <div className="registration-login-page-container">
-      <Row>
-        <Col>
-          <div className="form-box shadow" style={{ marginTop: "10%" }}>
-            <h1 style={{ textAlign: "center", fontWeight: "bold" }}>
-              Sign In.
-            </h1>
-            <Form>
-              <Form.Group controlId="">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  className="form-boxes"
-                  size="lg"
-                  placeholder="Enter email"
-                  value={inputs.email}
-                  onChange={onChangeInput}
-                />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+      <div className="form-box shadow">
+        <h1 style={{ textAlign: "center", fontWeight: "bold" }}>Sign In.</h1>
+        <Form>
+          <Form.Group controlId="">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              className="form-boxes"
+              size="lg"
+              placeholder="Enter email"
+              value={inputs.email}
+              onChange={onChangeInput}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-              <Form.Group controlId="">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  className="form-boxes"
-                  size="lg"
-                  placeholder="Password"
-                  value={inputs.password}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
-              <Form.Group controlId="">
-                <Form.Check
-                  type="checkbox"
-                  label="I do nothing, but you could still check me if you wish!"
-                />
-              </Form.Group>
+          <Form.Group controlId="">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              className="form-boxes"
+              size="lg"
+              placeholder="Password"
+              value={inputs.password}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="">
+            <Form.Check
+              type="checkbox"
+              label="I do nothing, but you could still check me if you wish!"
+            />
+          </Form.Group>
 
-              <div style={{ position: "absolute", top: "0", right: "35%" }}>
-                <Toast show={showErrorToast} onClose={closeToast}>
-                  <Toast.Header>
-                    <img
-                      src="holder.js/20x20?text=%20"
-                      className="rounded mr-2"
-                      alt=""
-                    />
-                    <strong className="mr-auto">Error Message</strong>
-                    <small>Just Now</small>
-                  </Toast.Header>
-                  <Toast.Body>{showErrorMessage}</Toast.Body>
-                </Toast>
-              </div>
-
-              <Button
-                variant="primary"
-                type="submit"
-                className="form-button"
-                onClick={formSubmit}
-              >
-                Sign In
-              </Button>
-            </Form>
+          <div style={{ position: "absolute", top: "0", right: "35%" }}>
+            <Toast show={showErrorToast} onClose={closeToast}>
+              <Toast.Header>
+                <img
+                  src="holder.js/20x20?text=%20"
+                  className="rounded mr-2"
+                  alt=""
+                />
+                <strong className="mr-auto">Error Message</strong>
+                <small>Just Now</small>
+              </Toast.Header>
+              <Toast.Body>{showErrorMessage}</Toast.Body>
+            </Toast>
           </div>
-        </Col>
-        <Col
-          sm="5"
-          className="d-none d-md-block"
-          style={{
-            height: "90vh",
-            borderTopRightRadius: "45%",
-            overflow: "hidden"
-          }}
-        >
-          <img
-            width="100%"
-            height="100%"
-            src="https://res.cloudinary.com/dcft8yhab/image/upload/v1573588097/undraw_zoom_in_1txs.png"
-          />
-        </Col>
-      </Row>
+
+          <Button
+            variant="primary"
+            type="submit"
+            className="form-button"
+            onClick={formSubmit}
+          >
+            Sign In
+          </Button>
+        </Form>
+        <p class="register-login-redirect">
+          Don't have an account yet? <Link to="/register">Register.</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Form, Container, Row, Col, Button, Toast } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import {Redirect} from "react-router-dom"
+import { Redirect } from "react-router-dom";
 import { registerUser } from "../actions/userActions";
 import "./css/forms.css";
 
-const Register = props => {
+const Register = (props) => {
   const dispatchRef = useDispatch();
   const [inputs, setInputs] = useState({
     firstname: "",
@@ -13,47 +14,49 @@ const Register = props => {
     email: "",
     phoneNumber: "",
     password: "",
-    confirmPassword: ""
+    confirmPassword: "",
   });
-  const onChangeInput = e => {
+  const onChangeInput = (e) => {
     const { name, value } = e.target;
     setInputs({ ...inputs, [name]: value });
     console.log(inputs.name);
   };
 
   const [showErrorToast, setShowErrorToast] = useState(false);
-  const [showErrorMessage, setShowErrorMessage] = useState('');
-  
-  const closeToast = () => {
-    setShowErrorToast(false)
-  }
+  const [showErrorMessage, setShowErrorMessage] = useState("");
 
-  const onSubmit = async e => {
+  const closeToast = () => {
+    setShowErrorToast(false);
+  };
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    if(!inputs.firstname){
+    if (!inputs.firstname) {
       setShowErrorMessage("A valid Firstname input is required!");
       setShowErrorToast(!showErrorToast);
-      return
-    }else if(!inputs.lastname){
+      return;
+    } else if (!inputs.lastname) {
       setShowErrorMessage("A valid Lastname input is required!");
       setShowErrorToast(!showErrorToast);
-      return
-    }else if(!inputs.email){
+      return;
+    } else if (!inputs.email) {
       setShowErrorMessage("A valid Email input is required!");
       setShowErrorToast(!showErrorToast);
-      return
-    }else if(!inputs.phoneNumber){
+      return;
+    } else if (!inputs.phoneNumber) {
       setShowErrorMessage("A valid Phone Number input is required!");
       setShowErrorToast(!showErrorToast);
-      return
-    }else if(!inputs.password){
+      return;
+    } else if (!inputs.password) {
       setShowErrorMessage("A valid Password input is required!");
       setShowErrorToast(!showErrorToast);
-      return
-    }else if(!inputs.confirmPassword){
-      setShowErrorMessage("Please Confirm Password input field cannot be left blank!");
+      return;
+    } else if (!inputs.confirmPassword) {
+      setShowErrorMessage(
+        "Please Confirm Password input field cannot be left blank!"
+      );
       setShowErrorToast(!showErrorToast);
-      return
+      return;
     }
 
     const {
@@ -62,153 +65,133 @@ const Register = props => {
       email,
       phoneNumber,
       password,
-      confirmPassword
+      confirmPassword,
     } = inputs;
     const registerData = {
       firstname: firstname,
       lastname: lastname,
       email: email,
       phoneNumber: phoneNumber,
-      password: password
+      password: password,
     };
     if (password != confirmPassword) {
-      setShowErrorMessage("The confirm password doesn't match the main password field")
-      setShowErrorToast(!showErrorToast)
+      setShowErrorMessage(
+        "The confirm password doesn't match the main password field"
+      );
+      setShowErrorToast(!showErrorToast);
       return;
     }
-    const emailPattern= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if(!emailPattern.test(email)){
-      setShowErrorMessage("The Email provided is not a valid email address")
-      setShowErrorToast(!showErrorToast)
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(email)) {
+      setShowErrorMessage("The Email provided is not a valid email address");
+      setShowErrorToast(!showErrorToast);
       return;
     }
     console.log(registerData);
     registerUser(registerData, dispatchRef);
-    return(
-      <Redirect to="/dashboard"/>
-    )
+    props.history.push("/dashboard");
   };
 
   return (
     <div className="registration-login-page-container">
-      <Row>
-        <Col
-          sm="5"
-          className="d-none d-md-block"
-          style={{
-            height: "90vh",
-            borderTopRightRadius: "45%",
-            overflow: "hidden"
-          }}
-        >
-          <img
-            width="100%"
-            height="100%"
-            src="https://res.cloudinary.com/dcft8yhab/image/upload/v1573588622/undraw_jason_mask_t07o.png"
-          />
-        </Col>
-        <Col>
-          <div className="form-box">
-            <h1 style={{ textAlign: "center", fontWeight: "bold" }}>
-              Sign Up.
-            </h1>
-            <Form>
-              <Form.Group controlId="">
-                <Form.Control
-                  className="form-boxes"
-                  type="text"
-                  name="firstname"
-                  size="lg"
-                  placeholder="First Name..."
-                  value={inputs.firstname}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
-              <Form.Group controlId="">
-                <Form.Control
-                  className="form-boxes"
-                  type="text"
-                  name="lastname"
-                  size="lg"
-                  placeholder="Last Name..."
-                  value={inputs.lastname}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
-              <Form.Group controlId="">
-                <Form.Control
-                  className="form-boxes"
-                  type="email"
-                  name="email"
-                  size="lg"
-                  placeholder="Email Address"
-                  value={inputs.email}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
-              <Form.Group controlId="">
-                <Form.Control
-                  className="form-boxes"
-                  type="number"
-                  name="phoneNumber"
-                  size="lg"
-                  placeholder="Phone Number..."
-                  value={inputs.phoneNumber}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
-              <Form.Group controlId="">
-                <Form.Control
-                  className="form-boxes"
-                  type="password"
-                  name="password"
-                  size="lg"
-                  placeholder="Enter Password..."
-                  value={inputs.password}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
-              <Form.Group controlId="">
-                <Form.Control
-                  className="form-boxes"
-                  type="password"
-                  name="confirmPassword"
-                  size="lg"
-                  placeholder="Confirm Password..."
-                  value={inputs.confirmPassword}
-                  onChange={onChangeInput}
-                />
-              </Form.Group>
+      <div className="form-box shadow">
+        <h1 style={{ textAlign: "center", fontWeight: "bold" }}>Sign Up.</h1>
+        <Form>
+          <Form.Group controlId="">
+            <Form.Control
+              className="form-boxes"
+              type="text"
+              name="firstname"
+              size="lg"
+              placeholder="First Name..."
+              value={inputs.firstname}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="">
+            <Form.Control
+              className="form-boxes"
+              type="text"
+              name="lastname"
+              size="lg"
+              placeholder="Last Name..."
+              value={inputs.lastname}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="">
+            <Form.Control
+              className="form-boxes"
+              type="email"
+              name="email"
+              size="lg"
+              placeholder="Email Address"
+              value={inputs.email}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="">
+            <Form.Control
+              className="form-boxes"
+              type="number"
+              name="phoneNumber"
+              size="lg"
+              placeholder="Phone Number..."
+              value={inputs.phoneNumber}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="">
+            <Form.Control
+              className="form-boxes"
+              type="password"
+              name="password"
+              size="lg"
+              placeholder="Enter Password..."
+              value={inputs.password}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="">
+            <Form.Control
+              className="form-boxes"
+              type="password"
+              name="confirmPassword"
+              size="lg"
+              placeholder="Confirm Password..."
+              value={inputs.confirmPassword}
+              onChange={onChangeInput}
+            />
+          </Form.Group>
 
-              <div style={{position: "absolute", top: "0", right: "35%"}}>
-                <Toast show={showErrorToast} onClose={closeToast}>
-                  <Toast.Header>
-                    <img
-                      src="holder.js/20x20?text=%20"
-                      className="rounded mr-2"
-                      alt=""
-                    />
-                    <strong className="mr-auto">Error Message</strong>
-                    <small>Just Now</small>
-                  </Toast.Header>
-                  <Toast.Body>
-                    {showErrorMessage}
-                  </Toast.Body>
-                </Toast>
-              </div>
-
-              <Button
-                className="form-button"
-                variant="primary"
-                type="submit"
-                onClick={onSubmit}
-              >
-                Register
-              </Button>
-            </Form>
+          <div style={{ position: "absolute", top: "0", right: "35%" }}>
+            <Toast show={showErrorToast} onClose={closeToast}>
+              <Toast.Header>
+                <img
+                  src="holder.js/20x20?text=%20"
+                  className="rounded mr-2"
+                  alt=""
+                />
+                <strong className="mr-auto">Error Message</strong>
+                <small>Just Now</small>
+              </Toast.Header>
+              <Toast.Body>{showErrorMessage}</Toast.Body>
+            </Toast>
           </div>
-        </Col>
-      </Row>
+
+          <Button
+            className="form-button"
+            variant="primary"
+            type="submit"
+            onClick={onSubmit}
+          >
+            Register
+          </Button>
+        </Form>
+        <p class="register-login-redirect">
+          Already have an account? <Link to="/login">Login.</Link>
+        </p>
+      </div>
     </div>
   );
 };
